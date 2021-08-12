27244418eea4eb18536c262b9a438f271991ec99
mvcbohyun <jbh959847@gmail.com>
2021-08-12 15:06:53 +0900

    엑셀로 크롤링 데이터 옮기기

--- xlsxindex.js
+++ xlsxindex.js
@@ -1,6 +1,7 @@
 const xlsx = require('xlsx');
 const axios = require('axios'); // ajax 라이브러리
 const cheerio = require('cheerio');// html 파싱
+const add_to_sheet = require('./add_to_sheet');// html 파싱
 
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 
@@ -24,16 +25,20 @@ const crawler = async ()=>{
     // 순서 대로 가져옴 밑에 코드보단 느림
     
     for(const[i,r] of records.entries()){
+        add_to_sheet(ws,'C1','s','평점')
         const response = await axios.get(r.링크);
         if(response.status ===200){// 응다입 성공한 경우
             const html = response.data;
             
             const $ = cheerio.load(html);
-            const text =$('.score.score_left .star_score').text();
+            const text =$('.score.score_left .star_score').text();// 태그는 무시하고 텍스트 값만 가져옴 
             console.log(r.제목 ,'평점 '+text.trim());
+            const  newCell = 'C'+(i+2);
+            add_to_sheet(ws,newCell,'n',parseFloat(text.trim()));
 
         }
     }
+    xlsx.writeFile(workbook ,'xlsx/result.xlsx');
     // 순서 상관없이 한번에 요청해서 가져옴  순서가 변경될수 있음 !
     // await Promise.all(records.map( async(r) =>{
        
